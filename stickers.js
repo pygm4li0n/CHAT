15c8638f056595fb8793cfb3bd6e9a4333967898
Marcelo Adornor <z0r4lity@gmail.com>
2026-09-23 20:24:34 -0300

    Add new sticker 'CLOWN' to stickers.js

--- stickers.js
+++ stickers.js
@@ -33,7 +33,8 @@
         { key: 'LOVE', url: 'https://i.postimg.cc/qRftPQd2/LOVE.png' },
         { key: 'MAD', url: 'https://i.postimg.cc/pTN9wktB/MAD.png' },
         { key: 'SLEEP', url: 'https://i.postimg.cc/zBmLsp1F/SLEEP.png' },
-        { key: 'WHAT', url: 'https://i.postimg.cc/26pqsTfv/WHAT.png' }
+        { key: 'WHAT', url: 'https://i.postimg.cc/26pqsTfv/WHAT.png' },
+        { key: 'CLOWN', url: 'https://i.postimg.cc/VNXnVVVf/Chat-GPT-Image-22-sept-2026-07-24-56-p-m-(1).png' }
     ];
 
     var GRID_SLOTS = 16;   // 4 × 4
